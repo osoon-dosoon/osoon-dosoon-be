70cd08233602270ebc30e090ba5017962e88b604
yjp8842 <yjp20183029@gmail.com>
2025-02-06 01:32:27 +0900

    init: 프로젝트 초기 생성

---
+++ src/main/java/com/example/osoondosoon/OsoondosoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.osoondosoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsoondosoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsoondosoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/osoondosoon/OsoondosoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.osoondosoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsoondosoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
